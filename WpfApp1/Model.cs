14806652190fe38c80de3cbf216b6d526bd0d298
helpdesk <helpdesk@TRS-HELPDESK>
2023-01-22 23:11:38 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+
+namespace WpfApp1
+{
+    public class Model 
+    {
+
+        // инициаилизуем наши "типо переменные, находящиеся на main window xaml"
+        public static string tbFirst;
+        public static string tbSecond;
+
+        // создаем лист, с данными, которые будут использоваться в комбобоксе в качестве выбранного элемента. такая же аналогия со знаками. взависимости от выбранного эл-та ко-кса меняется знак
+        public static List<string> cmbOperation = new List<string>() { "Сложение", "Умножение", "Деление", "Вычитание" };
+        public static List<string> tbSign = new List<string>() { "+", "*", "/", "-" };
+
+
+        public static TextBlock tbSignText = new TextBlock();
+        public static string tbResult = "";
+
+
+    }
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Navigation;
+using System.Windows.Input;
+using System.Collections;
+using System;
+
+namespace WpfApp1
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged; // реализовываем событие
+
+
+        public string tbSign { get => Model.tbSign.ToString(); }
+        public List<string> cmbItems { get; } = Model.cmbOperation;
+        //public List<string> tbSign { get; } = Model.tbSign;
+
+
+        int SelectionIndex = 0;
+        public int cbSelectedIndex
+        {
+            get
+            {
+                return SelectionIndex;
+            }
+            set
+            {
+                SelectionIndex = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("cbSelectedIndex2"));
+            }
+        }
+        public string cbSelectedIndex2
+        {
+            get
+            {
+                if (SelectionIndex == -1)
+                {
+                    return "No selected items";
+                }
+                else
+                {
+                    return Model.tbSign[SelectionIndex];
+
+                }
+            }
+
+
+        }
+        public string Result
+        {
+            get
+            {
+                return Model.tbResult.ToString();
+            }
+        }
+
+        public string tbFirst
+        {
+            set
+            {
+                Model.tbFirst = value;
+            }
+            get
+            {
+                return Model.tbFirst;
+            }
+        }
+        public string tbSecond
+        {
+            set
+            {
+                Model.tbSecond = value;
+            }
+            get
+            {
+                return Model.tbSecond;
+            }
+        }
+        public RoutedCommand Command { get; set; } = new RoutedCommand();
+        public void Command_Execude(object sender, ExecutedRoutedEventArgs e)
+        {
+            double firstVariable = Convert.ToDouble(Model.tbFirst);
+            double secondVariable = Convert.ToDouble(Model.tbSecond);
+
+
+            switch (SelectionIndex)
+            {
+                case 0: // кейсы - айдишники выбранного  элементы комбобокса
+                    Model.tbResult = Convert.ToString(firstVariable + secondVariable);
+
+                    break;
+                case 1:
+                    Model.tbResult = Convert.ToString(firstVariable * secondVariable);
+
+                    break;
+                case 2:
+                    if (secondVariable == 0)
+                    {
+                        Model.tbResult = "Вы что? Делить на 0 нельзя";
+
+                    }
+                    else
+                    {
+                        Model.tbResult = Convert.ToString(firstVariable / secondVariable);
+                    }
+                    break;
+                case 3:
+                    Model.tbResult = Convert.ToString(firstVariable - secondVariable);
+                    break;
+
+
+
+            }
+            PropertyChanged(this, new PropertyChangedEventArgs("Result"));
+        }
+        public CommandBinding binding;
+        public ViewModel()
+        {
+
+            binding = new CommandBinding(Command);
+            binding.Executed += Command_Execude;
+        }
+
+
+        // public List<string> tbSign { get; } = Model.tbSign;
+    }
+}
